da19802021aa0da85435b11d2f724290ac9aede1
Danil <the.clint.fi@gmail.com>
2021-12-17 17:02:27 +0300

    add_4

---
+++ Additions_4/add_4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const unsigned max_sum = 27;
+
+    unsigned sum[max_sum + 1] = { 0 };
+
+    for (int i = 0; i < 10; ++i)
+        for (int j = 0; j < 10; ++j)
+            for (int x = 0; x < 10; ++x)
+                ++sum[i + j + x];
+   
+
+    unsigned count = 0;
+    for (int i = 1; i <= max_sum; ++i)
+    {
+        count += sum[i] * sum[i];
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
